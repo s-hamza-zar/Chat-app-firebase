80822e2664d87a417c71d09ffbd582d52c7caa9d
s-hamza-zar <s.hamza.zar@gmail.com>
2021-09-04 20:22:35 +0500

    image url in db

--- signup/signup.js
+++ signup/signup.js
@@ -1,41 +1,6 @@
 let signUpform = document.querySelector("#form");
-signUpform.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  //user info
-  let email = document.querySelector("#emailId").value;
-  let userName = document.querySelector("#userNameId").value;
-  let password = document.querySelector("#passwordId").value;
-  let confirmPassword = document.querySelector("#ConfirmPasswordId").value;
-  console.log(email, userName, password);
-
-  if (!(password.trim() === confirmPassword.trim())) {
-    alert("Password donot match");
-    return;
-  }
-
-  auth
-    .createUserWithEmailAndPassword(email, password)
-    .then((credentials) => {
-      console.log(credentials.user);
-
-      db.collection("users")
-        .doc(credentials.user.uid)
-        .set({
-          email: email,
-          name: userName,
-          id: credentials.user.uid,
-        })
-        .then((response) => {
-          // location.assign("https://www.w3schools.com");
-          console.log("hamza")
-        });
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-    
-  let selectedfile;
+let image;
+let selectedfile;
   let picture=document.querySelector("#customFile")
   console.log("picture:",picture)
 
@@ -49,9 +14,58 @@ signUpform.addEventListener("submit", (e) => {
     
     
     })
-  var storageRef = storage.ref("/images/")
+
+signUpform.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //user info
+
+  let email = document.querySelector("#emailId").value;
+  let userName = document.querySelector("#userNameId").value;
+  let password = document.querySelector("#passwordId").value;
+  let confirmPassword = document.querySelector("#ConfirmPasswordId").value;
+  console.log(email, userName, password);
+
+  if (!(password.trim() === confirmPassword.trim())) {
+    alert("Password donot match");
+    return;
+  }
+  
+  let filename=selectedfile.name;
+  var storageRef = storage.ref("/images/"+filename);
   console.log("storagref:",storageRef)
   var uploadTask=storageRef.put(selectedfile);
   console.log("upload:",uploadTask)
+  
+  uploadTask.on('state_changed', 
+  (snapshot) =>{
+  snapshot.ref.getDownloadURL()
+  .then((downloadURL) => {
+    console.log('File available at', downloadURL);
+    image=downloadURL
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((credentials) => {
+      console.log(credentials.user);
+
+      db.collection("users")
+        .doc(credentials.user.uid)
+        .set({
+          email: email,
+          name: userName,
+          id: credentials.user.uid,
+          avatar:image
+          
+        })
+        .then((response) => {
+          // location.assign("https://www.w3schools.com");
+          console.log("hamza")
+        });
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+  });
+  });
 
 });
